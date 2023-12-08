a0b6cee8408948b875b530804d6ca4f24b0d58a8
Rasoul Vatanparast <24911048+rasoulvatanparast@users.noreply.github.com>
2023-12-08 11:04:01 +0330

    Activate Schema

---
+++ model/Activate.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ActivateSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+    maxlength: 255,
+  },
+
+  uuid: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  expiresAt: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: String,
+    required: true,
+  },
+
+  used: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Activate", ActivateSchema);
